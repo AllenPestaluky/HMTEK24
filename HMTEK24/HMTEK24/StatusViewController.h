6eaac303fe5448c702bf4e80796c30296e1a9961
AllenPestaluky <allenwp@live.ca>
2011-09-17 20:46:42 -0400

    updated main status view controller to have the new dynamic title for time remaining

--- HMTEK24/HMTEK24/StatusViewController.h
+++ HMTEK24/HMTEK24/StatusViewController.h
@@ -8,10 +8,15 @@
 
 #import <UIKit/UIKit.h>
 @class PlayerStatus;
+@class FontLabel;
 
 @interface StatusViewController : UIViewController {
   IBOutlet UIImageView *zombieBackImage;
   IBOutlet UIImageView *aliveBackImage;
+  
+  UILabel* timeRemainingLabel1;
+  UILabel* timeRemainingLabel2;
+  UILabel* timeRemainingLabel3;
 }
 - (void)applicationDidBecomeActive;
 - (void)refreshStatusView: (PlayerStatus*) status;
--- HMTEK24/HMTEK24/StatusViewController.m
+++ HMTEK24/HMTEK24/StatusViewController.m
@@ -10,6 +10,11 @@
 #import "PlayerStatus.h"
 #import "FontLabel.h"
 
+@interface StatusViewController()
+-(void) sizeLabel: (UILabel*) label;
+-(UILabel*) newLabel;
+@end
+
 @implementation StatusViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -36,18 +41,39 @@
     [super viewDidLoad];
   // Do any additional setup after loading the view from its nib.
   
-  FontLabel *label = [[FontLabel alloc] initWithFrame:CGRectMake(10, 10, 0, 0) fontName:@"PRISTINA" pointSize:40.0f];
-	label.textColor = [UIColor magentaColor];
-	label.text = @"lorem ipsum";
-	[label sizeToFit];
-	label.backgroundColor = nil;
-	label.opaque = NO;
-	[self.view addSubview:label];
-	[label release];
+  CGRect tempFrame;
+  int startingY = self.view.frame.size.height * 0.05;
+  int lineHeight = self.view.frame.size.height * 0.08;
+  
+  timeRemainingLabel1 = [self newLabel];
+  tempFrame = timeRemainingLabel1.frame;
+  tempFrame.origin.y = startingY;
+  timeRemainingLabel1.frame = tempFrame;
+  
+  timeRemainingLabel2 = [self newLabel];
+  tempFrame = timeRemainingLabel2.frame;
+  tempFrame.origin.y = startingY + lineHeight;
+  timeRemainingLabel2.frame = tempFrame;
+  
+  timeRemainingLabel3 = [self newLabel];
+  tempFrame = timeRemainingLabel1.frame;
+  tempFrame.origin.y = startingY + lineHeight *2;
+  timeRemainingLabel3.frame = tempFrame;
   
   PlayerStatus* status = [[[PlayerStatus alloc] init] autorelease];
-  status.isZombie = (rand() % 2 == 0) ? YES : NO;
+  status.isZombie = NO;
   [self refreshStatusView:status];
+}
+
+-(UILabel*) newLabel {
+  UILabel* label = [[FontLabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 0) fontName:@"PRISTINA" pointSize:60.0f];
+	label.textColor = [UIColor redColor];
+	label.backgroundColor = nil;
+	label.opaque = NO;
+  label.numberOfLines = 0; // any number of lines
+  label.textAlignment = UITextAlignmentCenter;
+	[self.view addSubview:label];
+  return label;
 }
 
 - (void)viewDidUnload
@@ -56,6 +82,14 @@
   aliveBackImage = nil;
   [zombieBackImage release];
   zombieBackImage = nil;
+  
+  [timeRemainingLabel1 release];
+  timeRemainingLabel1 = nil;
+  [timeRemainingLabel2 release];
+  timeRemainingLabel2 = nil;
+  [timeRemainingLabel3 release];
+  timeRemainingLabel3 = nil;
+  
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -77,6 +111,9 @@
   // First hide everything:
   zombieBackImage.hidden = YES;
   aliveBackImage.hidden = YES;
+  timeRemainingLabel1.hidden = YES;
+  timeRemainingLabel2.hidden = YES;
+  timeRemainingLabel3.hidden = YES;
   
   if(status.isZombie) {
     zombieBackImage.hidden = NO;
@@ -84,7 +121,25 @@
   } else {
     aliveBackImage.hidden = NO;
     
+    timeRemainingLabel1.hidden = NO;
+    timeRemainingLabel1.text = [NSString stringWithFormat: @"%i hours", 12];
+    [self sizeLabel: timeRemainingLabel1];
+    
+    timeRemainingLabel2.hidden = NO;
+    timeRemainingLabel2.text = [NSString stringWithFormat: @"%i minutes", 32 ];
+    [self sizeLabel: timeRemainingLabel2];
+    
+    timeRemainingLabel3.hidden = NO;
+    timeRemainingLabel3.text = [NSString stringWithFormat: @"remain"];
+    [self sizeLabel: timeRemainingLabel3];
   }
+}
+
+-(void) sizeLabel: (UILabel*) label {
+  [label sizeToFit];
+  CGRect newFrame = label.frame;
+  newFrame.origin.x = self.view.frame.size.width / 2 - newFrame.size.width / 2;
+  label.frame = newFrame;
 }
 
 - (void)dealloc {
